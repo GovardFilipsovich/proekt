edcffd89a2f6459cbc6a6c447f49e04aee107ca5
Richard Clark <gajkin.kirill@yandex.ru>
2021-01-17 14:19:11 -0500

    Изменен класс Arcade. Теперь мяч изменяет скорость в зависимости от очков

--- arcade.py
+++ arcade.py
@@ -1,8 +1,18 @@
 from breakout import Breakout
 
 class Arcade(Breakout):
+    count = 0
+    sc = -1
     def update(self):
         if not self.bricks:
             self.create_bricks()
+            sp = self.ball.speed
+            self.objects.remove(self.ball)
+            self.create_ball()
+            self.ball.speed = sp
             return
         super().update()
+        print(self.ball.speed, self.score)
+        if self.score % 5 == 0 and self.sc != self.score:
+            self.sc = self.score
+            self.ball.speed = (self.ball.speed[0], self.ball.speed[1] + 1)
